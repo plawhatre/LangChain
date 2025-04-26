efd8306012543a1419a3594f7c09459ecf653f97
prashant <prashantlawhatre@gmail.com>
2025-04-26 18:23:25 +0530

    added tuple method

--- messages.py
+++ messages.py
@@ -1,8 +1,14 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage, HumanMessage
 
-# MessagesPlaceholder, SystemMessage, HumanMessage
+# MessagesPlaceholder, SystemMessage, HumanMessage (with functions)
 system_message = SystemMessage("You are a helpful assistant")
-placeholder = MessagesPlaceholder("inp")
-prompt_template = ChatPromptTemplate.from_messages([system_message, placeholder])
+placeholder_message = MessagesPlaceholder("inp")
+prompt_template = ChatPromptTemplate.from_messages([system_message, placeholder_message])
+print(prompt_template.invoke({"inp": [HumanMessage("Tell me a joke about Snakes")]}))
+
+# MessagesPlaceholder (with tuples)
+system_message = ("system", "You are a helpful assistant")
+placeholder_message = ("placeholder", "{inp}")
+prompt_template = ChatPromptTemplate.from_messages([system_message, placeholder_message])
 print(prompt_template.invoke({"inp": [HumanMessage("Tell me a joke about Snakes")]}))
