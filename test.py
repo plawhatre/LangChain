aa6a321638643133c4b5e7fbc12b015816b5456c
prashant <prashantlawhatre@gmail.com>
2025-04-26 21:16:10 +0530

    added api call option

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
+import os
+from getpass import getpass
 from langchain_ollama import ChatOllama
-
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 if __name__ == '__main__':
-    # model
-    llm = ChatOllama(
-        model = "mistral:v0.3",
-        temperature = 0.8,
-        num_predict = 256,
-    )
+    run_local = True
+    if run_local:
+        # Mistral-7b-v0.3 or deepseek-r1:1.5b
+        llm = ChatOllama(
+            # model="mistral:v0.3",
+            model="deepseek-r1:1.5b",
+            temperature=0.8,
+            num_predict=256,
+        )
+    else:
+        # Gemini 2.0 Flash
+        if 'GOOGLE_API_KEY' not in os.environ:
+            os.environ['GOOGLE_API_KEY'] = getpass("Enter the API key: ")
+        llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', temperature = 0.8)
 
     # prompt
     messages = [
@@ -16,4 +26,4 @@ if __name__ == '__main__':
     ]
 
     # generate response
-    print(llm.invoke(messages))
+    print(llm.invoke(messages).content)
