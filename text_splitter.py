1ab4c5f72a7d150586cfb22d4d6dea34a47141b1
prashant <prashantlawhatre@gmail.com>
2025-05-07 20:22:15 +0530

    adde text splitter

---
+++ text_splitter.py
@@ -0,0 +1,26 @@
+import os
+import httpx
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+if __name__ == '__main__':
+
+    # Step1: Load the documents
+    url = "https://arxiv.org/pdf/1706.03762"
+    filename = "attention_is_all_you_need.pdf"
+
+    if not os.path.isfile(filename):
+        content = httpx.get(url).content
+        with open(filename, 'wb') as file:
+            file.write(content)
+
+    documents = PyPDFLoader(filename).load()
+
+    # Step2: Split the document
+    all_splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        add_start_index=True
+    ).split_documents(documents)
+
+    print(all_splits[0].page_content)
