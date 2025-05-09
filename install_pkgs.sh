164edfd821445a192183f8c75de774e455e7b8d7
prashant <prashantlawhatre@gmail.com>
2025-05-09 21:11:51 +0530

    added chroma db related packages

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -3,5 +3,8 @@ source env_langchain/bin/activate
 # Install the required Python packages
 python3 -m pip install -r requirements.txt
 python3 -m pip install langchain-community pypdf
-python3 -m pip install langchain-chroma
 python -m pip install faiss-cpu
+# vector db
+pip install --upgrade setuptools
+pip install protobuf==6.30.0
+python3 -m pip install langchain-chroma>=0.1.2
