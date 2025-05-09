487e29568362681c13c5636889f3b1fcfdbe8ac8
prashant <prashantlawhatre@gmail.com>
2025-05-09 19:43:12 +0530

    added chroma db for self query retriever

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -3,4 +3,5 @@ source env_langchain/bin/activate
 # Install the required Python packages
 python3 -m pip install -r requirements.txt
 python3 -m pip install langchain-community pypdf
+python3 -m pip install langchain-chroma
 python -m pip install faiss-cpu
